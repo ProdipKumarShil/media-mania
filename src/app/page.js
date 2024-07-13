42acdca28c759ed41e432a3bd4754933832cdd7f
Prodip Kumar Shil <prodipkrishna01@gmail.com>
2024-07-14 00:18:25 +0600

    merge form development[3]

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,38 @@
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
-import { Button } from "@/components/ui/button";
-import connect from "@/lib/mongoose";
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
+import AllBlogs from "@/components/homeComponent/AllBlogs/AllBlogs";
+import Banner from "@/components/homeComponent/Banner";
+import RecentBlogs from "@/components/homeComponent/RecentBlogs";
 
 export default function Home() {
   return (
     <div>
-      <p>Hello from next js</p>
+      <div className=' py-10 m-10'>
+        <Banner />
+        <RecentBlogs />
+        <AllBlogs />
+        <Pagination >
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">2</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">3</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
     </div>
   );
 }
