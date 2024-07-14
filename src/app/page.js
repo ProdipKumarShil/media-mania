50eef27fe19fa13165b68f50ff89c792a09f2af1
Prodip Kumar Shil <prodipkrishna01@gmail.com>
2024-07-14 23:38:07 +0600

    remove double homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,42 @@
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
-import { Button } from "@/components/ui/button";
-import connect from "@/lib/mongoose";
+import AllBlogs from "@/components/homeComponent/AllBlogs/AllBlogs";
+import Banner from "@/components/homeComponent/Banner";
+import RecentBlogs from "@/components/homeComponent/RecentBlogs";
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
 
-export default function Home() {
-  return (
-    <div>
-      <p>Hello from next js</p>
-    </div>
-  );
-}
+
+const page = () => {
+    return (
+      <div className=' py-10 m-10'>
+       
+      <Banner/>
+      <RecentBlogs/>
+      <AllBlogs/>
+
+      <Pagination >
+  <PaginationContent>
+    <PaginationItem>
+      <PaginationPrevious href="#" />
+    </PaginationItem>
+    <PaginationItem>
+      <PaginationLink href="#">1</PaginationLink>
+    </PaginationItem>
+    <PaginationItem>
+      <PaginationLink href="#">2</PaginationLink>
+    </PaginationItem>
+    <PaginationItem>
+      <PaginationLink href="#">3</PaginationLink>
+    </PaginationItem>
+    <PaginationItem>
+      <PaginationEllipsis />
+    </PaginationItem>
+    <PaginationItem>
+      <PaginationNext href="#" />
+    </PaginationItem>
+  </PaginationContent>
+</Pagination>
+
+      </div>
+    );
+};
+
+export default page;
